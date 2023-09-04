46299fb41c2197735014d420c9fca059e1540bc4
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-09-04 13:03:52 +0100

    changed the navigation bar of the index page

--- pages/index.php
+++ pages/index.php
@@ -4,32 +4,38 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/index.css">
     <script src="https://kit.fontawesome.com/e9f64432b6.js" crossorigin="anonymous"></script>
     <title>Welcome page</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+            <a href="" class="btn btn-primary">JobHunt</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto" id="navbar">
+                <li class="nav-item  mx-1 my-1">
+                    <a href="" class="btn btn-outline-primary"><i class="fa fa-house"></i> Home</a>
+                </li>
+                <li class="nav-item mx-1 my-1">
+                    <a href="" class="btn btn-outline-primary"><i class="fa fa-gear"></i> Services</a>
+                </li>
+                <li class="nav-item mx-1 my-1">
+                    <a href="" class="btn btn-outline-primary"><i class="fa fa-users"></i> About</a>
+                </li>
+            
+                <li class="nav-item mx-1 my-1">
+                    <a href="" class="btn btn-outline-primary"><i class="fa fa-phone"></i> Contact Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i></a>
+                        <a class=" btn btn-outline-primary" aria-current="page" href="#"><i class="fa fa-right-to-bracket "></i> SignIn</a>
                     </li>
-                    <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-gear"></i></a>
-                    </li> 
-                    <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-users"></i></a>
-                    </li>  
-                    <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-right-to-bracket"></i></a>
-                    </li>  
-                    <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-phone"></i></a>
-                    </li>
-                </ul>
+            </ul>
             </div>
         </div>
     </nav>
